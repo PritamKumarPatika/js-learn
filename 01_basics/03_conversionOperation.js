8e6cc87ea32e3d911a4926b3f974b1d9408c433b
PritamKumarPatika <165075359+PritamKumarPatika@users.noreply.github.com>
2025-02-19 07:03:33 +0000

    data conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+let score = "99";
+
+console.log(typeof(score));
+
+let ValueInNumber = Number(score);
+console.log(typeof(ValueInNumber));
+
+let isLoggedIn = 0;
+let booleanIsLoggedin = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedin);
